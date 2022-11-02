d305f12e6fca431acd9f17d708c9fc530d2d92d6
fazlur <fazlur@9yardstechnology.com>
2022-11-02 14:10:39 +0530

    new changes

---
+++ models/user.js
@@ -0,0 +1,31 @@
+// const Sequelize = require("sequelize");
+
+// const sequelize = require("../util/database");
+
+// const User = sequelize.define("user", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   email: {
+//     type: Sequelize.STRING(100),
+//     defaultValue: "",
+//     allowNull: false,
+//   },
+//   password: {
+//     type: Sequelize.STRING(100),
+//     allowNull: false,
+//   },  
+//   firstName: {
+//     type: Sequelize.STRING(100),
+//     allowNull: true,
+//   },
+//   lastName: {
+//     type: Sequelize.STRING(100),
+//     allowNull: true,
+//   },
+// });
+
+// module.exports = User;
---
+++ util/database.js
@@ -0,0 +1,11 @@
+// const Sequelize = require('sequelize');
+
+// // console.log('DB host ' + process.env.DB_HOST);
+// const sequelize = new Sequelize('9ythrms', 'root','9ytHRMS@123', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+
+
+
+// module.exports = sequelize;
