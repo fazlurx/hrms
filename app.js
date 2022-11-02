792fcafe4d6f7f45379ba1c2007d8ef06ac24596
fazlur <fazlur@9yardstechnology.com>
2022-11-02 16:25:44 +0530

    new code addeddon number 17878

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.set('view engine', 'jade');
 
 
 app.use(logger('dev'));
+app.use(logger('dev'));
+app.use(logger('dev'));
+app.use(logger('dev'));
+app.use(logger('dev'));
+app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
