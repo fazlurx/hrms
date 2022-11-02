9d7da2e9853f483b9be67757c1e821db9026b48c
fazlur <fazlur@9yardstechnology.com>
2022-11-02 16:07:03 +0530

    new code added lallantop

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade'); 
+app.set('view engine', 'jade'); 
+app.set('view engine', 'jade'); 
+app.set('view engine', 'jade'); 
 
 app.use(logger('dev'));
 app.use(express.json());
